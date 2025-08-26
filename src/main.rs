419ed800cad9a9635289bed48081aceac2be8e10
Udhayan K <kudhayan27@gmail.com>
2025-08-26 10:20:09 +0530

    Refactor number guessing game logic and improve readability

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
       println!("Guess the number");
 
-      let rng = rand::rng().random_range(0..=100);  
+      let mut rng = rand::rng();
 
-      println!("The Secret Number is :{}",rng);
+      let secret_number:u32 = rng.random_range(0..=100);  
 
-      println!("Please input your guess");
+      //println!("The Secret Number is :{}",secret_number);
+
+      loop{
+            println!("Please input your guess: ");
       
-      let mut guess =String::new();
-      
-      io::stdin().read_line(&mut guess).expect("Failed to read Line");
+            let mut guess =String::new();
+            
+            io::stdin().read_line(&mut guess).expect("Failed to read Line");
 
-      print!("You guessed :{}",guess);
+            let guess:u32 = match guess.trim().parse() {
+                  Ok(num) => num,
+                  Err(_) => continue
+            };
+
+            println!("You guessed: {guess}");
+
+            match guess.cmp(&secret_number){
+                  Ordering::Less => println!("Too Small!"),
+                  Ordering::Greater => println!("Too Big!"),
+                  Ordering::Equal => {
+                              println!("You Win, Perfect Guess!");
+                              break;
+                        }                  
+            }
+      }
 }
